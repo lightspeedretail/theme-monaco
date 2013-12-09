8f1bdf5600bb4614efa60bc91458ad718c651364
Arman Keyvanskhou <arman.keyvan@gmail.com>
2013-12-09 14:04:50 -0800

    Implemented header

--- views/site/_footer.php
+++ views/site/_footer.php
@@ -35,32 +35,4 @@
 
 	</div>
 
-
-
-
-<!-- 
-	<div class="span12">
-		<div class="addresshours">
-			<div class="span3 indentl responsive-break">
-				<?php
-				echo _xls_get_conf('STORE_NAME')."<br>";
-				echo _xls_get_conf('STORE_ADDRESS1')."<br>";
-				echo _xls_get_conf('STORE_ADDRESS2');
-				?>
-			</div>
-			<div class="span6 footertabs responsive-break">
-				<?php
-					foreach (CustomPage::model()->bottomtabs()->findAll() as $arrTab) {
-						echo CHtml::link(Yii::t('global',$arrTab->title),$arrTab->Link).' / ';						
-					}
-					echo CHtml::link(Yii::t('global','Sitemap'),$this->createUrl('site/map'));
-				?>
-
-			</div>
-		</div>
-		<div class="copyright">
-			&copy; <?= Yii::t('global', 'Copyright') ?> <?= date("Y"); ?> <?= _xls_get_conf('STORE_NAME') ?>. <?= Yii::t('global', 'All Rights Reserved'); ?>.
-		</div>
-	</div> -->
 </div>
-
