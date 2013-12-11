b2aa16178ad14bcd4c36360ba7d33fc8725b4b1a
Arman Keyvanskhou <arman.keyvan@gmail.com>
2013-12-11 11:22:33 -0800

    Added a container-fluid to the shopping cart

--- views/cart/index.php
+++ views/cart/index.php
@@ -30,108 +30,108 @@ $form = $this->beginWidget('CActiveForm', array(
 	'action'=>array('cart/updatecart'),
 
 ));?>
-
-<?php
-/*
- * This file is used in a renderPartial() to display the cart within another view
- * Because our cart is pulled from the component, we can render from anywhere
- *
- * If our controller set intEditMode to be true, then this becomes an edit form to let the user change qty
- */
-    if (!isset($model)) $model = Yii::app()->shoppingcart;
-?>
-
-<?php $this->renderPartial('/cart/_cartitems'); ?>
-
-<div class='row-fluid cart-adjust'>
-	<div class='span4'>
-	<?php echo CHtml::ajaxButton(
-		Yii::t('cart', 'Update Cart'),
-		array('cart/updatecart'),
-		array('data'=>'js:$("#ShoppingCart").serialize()',
-			'type'=>'POST',
-			'dataType'=>'json',
-			'success' => 'js:function(data){
-	                    if (data.action=="alert") {
-	                      alert(data.errormsg);
-						} else if (data.action=="success") {
-							 location.reload();
-						}}'
-		)); ?>
-		/
-		<?php echo CHtml::ajaxButton(
-		Yii::t('cart', 'Clear Cart'),
-		array('cart/clearcart'),
-		array('data'=>array(),
-			'type'=>'POST',
-			'dataType'=>'json',
-			'success' => 'js:function(data){
-	                    if (data.action=="alert") {
-	                      alert(data.errormsg);
-						} else if (data.action=="success") {
-							 location.reload();
-						}}'
-		),array('confirm'=>Yii::t('cart',"Are you sure you want to erase your cart items?"))); ?>
-	</div>
-	<div class='offset6 span1'>
-		<?= Yii::t('cart','Subtotal'); ?>
-	</div>
-	<div class='span1'>
-		 <?= _xls_currency($model->subtotal); ?>
-	</div>
-</div>
-<?php if($model->TaxTotal && Yii::app()->params['TAX_INCLUSIVE_PRICING']=='0'): ?>
-	<?php foreach($model->Taxes as $tax=>$taxvalue): ?>
-		<?php if($taxvalue): ?>
-		<div class='row-fluid'>
-            <div class='offset10 span1'>
-                <?= $tax; ?>
-            </div>
-            <div class='span1'>
-            	<?= _xls_currency($taxvalue); ?>
-            </div>
-        </div>
-		<?php endif; ?>
-	<?php endforeach; ?>
-<?php endif; ?>
-<div class='row-fluid'>
-	<div class='offset10 span1'>
-		<?= Yii::t('cart','Shipping'); ?>
-	</div>
-	<div class='span1'>
-		<?= _xls_currency($model->shipping_sell); ?>
-	</div>
-</div>
-<div class='row-fluid'>
-	<div class='offset10 span1'>
-		<?= Yii::t('cart',"Total"); ?> 
-	</div>
-	<div class='span1'>
-		<?= _xls_currency($model->total); ?>
-	</div>
-</div>
-<?php if($model->PromoCode): ?>
-    <div class="row-fluid remove-bottom">
-         <div class="span4 offset6 promoCode"><?= Yii::t('cart',"Promo Code {code} Applied",array('{code}'=>"<strong>".$model->PromoCode."</strong>")); ?>
-         </div>
-    </div>    
-<?php endif; ?>
-
-
-<div class="row-fluid">
-
-	<?php echo CHtml::link(Yii::t('cart','Checkout'),array('cart/checkout'), array('class' => 'checkout big-button')) ?>
-
-	<?= CHtml::tag('div',array(
-		'id'=>'shoppingcartcontinue',
-		'class'=>'span4 checkoutlink',
-		'style' => 'float: right;',
-		'onClick'=>'js:window.location.href="'. $this->returnUrl.'"'),
-		Yii::t('cart','Continue Shopping'));
+<div class='container-fluid'>
+	<?php
+	/*
+	 * This file is used in a renderPartial() to display the cart within another view
+	 * Because our cart is pulled from the component, we can render from anywhere
+	 *
+	 * If our controller set intEditMode to be true, then this becomes an edit form to let the user change qty
+	 */
+	    if (!isset($model)) $model = Yii::app()->shoppingcart;
 	?>
 
-</div>
+	<?php $this->renderPartial('/cart/_cartitems'); ?>
 
+	<div class='row-fluid cart-adjust'>
+		<div class='span4'>
+		<?php echo CHtml::ajaxButton(
+			Yii::t('cart', 'Update Cart'),
+			array('cart/updatecart'),
+			array('data'=>'js:$("#ShoppingCart").serialize()',
+				'type'=>'POST',
+				'dataType'=>'json',
+				'success' => 'js:function(data){
+		                    if (data.action=="alert") {
+		                      alert(data.errormsg);
+							} else if (data.action=="success") {
+								 location.reload();
+							}}'
+			)); ?>
+			/
+			<?php echo CHtml::ajaxButton(
+			Yii::t('cart', 'Clear Cart'),
+			array('cart/clearcart'),
+			array('data'=>array(),
+				'type'=>'POST',
+				'dataType'=>'json',
+				'success' => 'js:function(data){
+		                    if (data.action=="alert") {
+		                      alert(data.errormsg);
+							} else if (data.action=="success") {
+								 location.reload();
+							}}'
+			),array('confirm'=>Yii::t('cart',"Are you sure you want to erase your cart items?"))); ?>
+		</div>
+		<div class='offset6 span1'>
+			<?= Yii::t('cart','Subtotal'); ?>
+		</div>
+		<div class='span1'>
+			 <?= _xls_currency($model->subtotal); ?>
+		</div>
+	</div>
+	<?php if($model->TaxTotal && Yii::app()->params['TAX_INCLUSIVE_PRICING']=='0'): ?>
+		<?php foreach($model->Taxes as $tax=>$taxvalue): ?>
+			<?php if($taxvalue): ?>
+			<div class='row-fluid'>
+	            <div class='offset10 span1'>
+	                <?= $tax; ?>
+	            </div>
+	            <div class='span1'>
+	            	<?= _xls_currency($taxvalue); ?>
+	            </div>
+	        </div>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	<?php endif; ?>
+	<div class='row-fluid'>
+		<div class='offset10 span1'>
+			<?= Yii::t('cart','Shipping'); ?>
+		</div>
+		<div class='span1'>
+			<?= _xls_currency($model->shipping_sell); ?>
+		</div>
+	</div>
+	<div class='row-fluid'>
+		<div class='offset10 span1'>
+			<?= Yii::t('cart',"Total"); ?> 
+		</div>
+		<div class='span1'>
+			<?= _xls_currency($model->total); ?>
+		</div>
+	</div>
+	<?php if($model->PromoCode): ?>
+	    <div class="row-fluid remove-bottom">
+	         <div class="span4 offset6 promoCode"><?= Yii::t('cart',"Promo Code {code} Applied",array('{code}'=>"<strong>".$model->PromoCode."</strong>")); ?>
+	         </div>
+	    </div>    
+	<?php endif; ?>
+
+
+	<div class="row-fluid">
+
+		<?php echo CHtml::link(Yii::t('cart','Checkout'),array('cart/checkout'), array('class' => 'checkout big-button')) ?>
+
+		<?= CHtml::tag('div',array(
+			'id'=>'shoppingcartcontinue',
+			'class'=>'span4 checkoutlink',
+			'style' => 'float: right;',
+			'onClick'=>'js:window.location.href="'. $this->returnUrl.'"'),
+			Yii::t('cart','Continue Shopping'));
+		?>
+
+	</div>
+</div>
 <?php $this->endWidget();
 
 
